637dbc61e5f2b4bc1804002e3204940338bef362
유채원 <ohwow5@sookmyung.ac.kr>
2020-01-09 14:32:43 +0900

    Add Asking for Permissions & Getting the Location

--- PROJECT/Weather-App/App.js
+++ PROJECT/Weather-App/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Alert} from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "red",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    color: "white"
+export default class extends React.Component  {
+  state = {
+    isLoading: true
   }
-});
+
+  getLocation = async() =>{
+    try{
+      await Location.requestPermissionsAsync();
+      const {coords : { latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.setState({isLoading : false});
+      //send to API and get Weather
+
+    }catch(error){
+      Alert.alert("Can't find you." , "So sad");
+
+    }
+    
+  }
+
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const {isLoading} = this.state;
+    return isLoading ? <Loading /> : null;
+  }
+}
