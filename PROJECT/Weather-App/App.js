21257802254af3305a396a4c7cef246ee2140d4f
유채원 <ohwow5@sookmyung.ac.kr>
2020-01-09 19:07:57 +0900

    add Background gradient & title and subTitle

--- PROJECT/Weather-App/App.js
+++ PROJECT/Weather-App/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
-import Weather from "./Weather";
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
 
 const API_KEY = "d5c7b2dfa7189541bbee24a5187b0c19";
 
@@ -32,22 +32,24 @@ export default class extends React.Component {
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
-      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      const {
+         coords: { latitude, longitude } 
+        } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude)
-      this.setState({ isLoading: false });
-      //send to API and get Weather
-
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
-
     }
-  }
+  };
 
   componentDidMount() {
     this.getLocation();
   }
   render() {
     const { isLoading, temp, condition } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
--- PROJECT/Weather-App/Weather.js
+++ PROJECT/Weather-App/Weather.js
@@ -1,59 +1,143 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Weather({ temp }) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.halfContainer}>
-                <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
-                <Text style={styles.temp}>{temp}º</Text>
-            </View>
-            <View style={styles.halfContainer}>
-
-            </View>
-
-        </View>
-    );
-}
-
-Weather.PropTypes = {
-    temp: PropTypes.number.isRequired,
-    condition: PropTypes.oneOf([
-        "Thunderstorm",
-        "Drizzle",
-        "Rain",
-        "Snow",
-        "Atmosphere",
-        "Clear",
-        "Clouds",
-        "Haze",
-        "Mist",
-        "Smoke	",
-        "Dust",
-        "Fog",
-        "Sand",
-        "Ash",
-        "Squall",
-        "Tornado",
-    ]).isRequired
-
+const weatherOptions = {
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#000000", "#434343"],
+    title: "지금날씨,",
+    subtitle: "천둥이다 ⚡️"
+  },
+  Drizzle: {
+    iconName: "weather-hail",
+    gradient: ["#d7d2cc", "#304352"],
+    title: "지금날씨,",
+    subtitle: "부슬부슬 💧"
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#24C6DC", "#514A9D"],
+    title: "지금날씨, ",
+    subtitle: "비온다 🌧"
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#2980B9", "#6DD5FA", "#FFFFFF"],
+    title: "지금날씨, ",
+    subtitle: "눈온다 ☃️"
+  },
+  Atmosphere: {
+    iconName: "weather-hail",
+    gradient: ["#2c3e50", "#bdc3c7"],
+    title: "지금날씨, ",
+    subtitle: "흐리다 🌫"
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#5B86E5", "#36D1DC"],
+    title: "지금날씨,",
+    subtitle: "맑다 ☀️"
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#2c3e50", "#2980b9"],
+    title: "지금날씨",
+    subtitle: ", 구름이다 ☁️"
+  },
+  Mist: {
+    iconName: "weather-hail",
+    gradient: ["#83a4d4", "#b6fbff"],
+    title: "지금날씨,",
+    subtitle: "습하다 💦"
+  },
+  Dust: {
+    iconName: "weather-hail",
+    gradient: ["#00223E", "#FFA17F"],
+    title: "지금날씨,",
+    subtitle: "우엑 먼지다 🤬"
+  },
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["#00416A", "#E4E5E6"],
+    title: "지금날씨, ",
+    subtitle: "안개 😎"
+  }
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    temp:{
-        fontSize: 42
+export default function Weather({ temp, condition }) {
+  return (
+    <LinearGradient
+      colors={weatherOptions[condition].gradient}
+      style={styles.container}
+    >
+      <StatusBar barStyle="light-content" />
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          size={96}
+          name={weatherOptions[condition].iconName}
+          color="white"
+        />
+        <Text style={styles.temp}>{temp}°</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherOptions[condition].subtitle}
+        </Text>
+      </View>
+    </LinearGradient>
+  );
+}
 
-    },
-    halfContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
+Weather.propTypes = {
+    temp: PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+      "Thunderstorm",
+      "Drizzle",
+      "Rain",
+      "Snow",
+      "Atmosphere",
+      "Clear",
+      "Clouds",
+      "Haze",
+      "Mist",
+      "Dust"
+    ]).isRequired
+  };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  temp: {
+    fontSize: 42,
+    color: "white"
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    fontWeight: "600",
+    color: "white",
+    fontSize: 24,
+    marginBottom: 10,
+    textAlign: "left"
+  },
+  subtitle: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: "800",
+    textAlign: "left"
+  },
+  textContainer: {
+    alignItems: "flex-start",
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    flex: 1
+  }
 });
